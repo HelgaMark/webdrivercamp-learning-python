891ca9e378448f6b822d4c09ebae8c949be67907
ruslan osipov <Portnov@DESKTOP-JICKLME.(none)olgamarkovaolga>
2024-05-20 21:51:15 -0700

    Task 3

---
+++ python_beginning/2_modify_numbers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+x = 3.141592653589793238
+print(f'Learning Python is fun"\' - {x:.2f} %')
+print('Learning Python is fun"\' - {:.2f} %'.format(x))
+print('Learning Python is fun"\' - %.2f %%' % x)
+
+
+
+
