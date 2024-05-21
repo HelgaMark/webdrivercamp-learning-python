93d0dc2faef921975942cfc9817e026177a201af
ruslan osipov <Portnov@DESKTOP-JICKLME.(none)olgamarkovaolga>
2024-05-20 23:10:20 -0700

    Task 7

---
+++ python_beginning/6_what_needed.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+x = "There should be one-- and preferably only one --obvious way to do it. lthough that way may not be obvious at first unless you're Dutch."
+
+y = x.replace("one ", "one way unless")
