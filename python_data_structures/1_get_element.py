3d834deb0da4f3b28b932e0f03b432f9fce7f45d
ruslan osipov <Portnov@DESKTOP-JICKLME.(none)olgamarkovaolga>
2024-05-27 17:19:27 -0700

    1_get_element.py

---
+++ python_data_structures/1_get_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+list_ = [5, 4, 3, 2, 1]
+index = 2
+
+# ADD YOUR CODE HERE
+if index < 0 or index > len(list_):
+    print("The element retrieved is None")
+
+else:
+    print("The element retrived is", list_[index])
