cbdce813d0336b73f299e8acdc2c5eb74f967a19
ruslan osipov <Portnov@DESKTOP-JICKLME.(none)olgamarkovaolga>
2024-05-28 23:02:42 -0700

    2_element_replaced.py

---
+++ python_data_structures/2_element_replaced.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+list_ = [5, 4, 3, 2, 1]
+index = 1
+element_to_replace = 5
+
+# ADD YOUR CODE HERE
+
+if index < 0 or index >= len(list_):
+    print(list_)
+    
+else:
+    list_[index] = element_to_replace
+    print(list_)
+ 
